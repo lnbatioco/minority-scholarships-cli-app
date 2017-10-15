81a6e30554b6c352061fb73a91c3e6e2717fc925
Lory NPC <loryleenbatioco@gmail.com>
2017-10-15 11:28:00 -0700

    add method that loads and prints minority groups

--- lib/minority-scholarships-cli-app/cli.rb
+++ lib/minority-scholarships-cli-app/cli.rb
@@ -1,11 +1,18 @@
 class MinorityScholarshipsCliApp::CLI
   def call
     welcome
+    print_minority_groups
   end
 
   def welcome
     puts "------------------------------- Welcome! -------------------------------"
     puts "Here is a list of scholarships offered to minority students."
     puts "Scholarship providers encourage ALL minority students to apply."
+    puts ""
+  end
+
+  def print_minority_groups
+    MinorityScholarshipsCliApp::Scraper.make_minority_groups
+    MinorityScholarshipsCliApp::MinorityGroups.print_minority_groups
   end
 end

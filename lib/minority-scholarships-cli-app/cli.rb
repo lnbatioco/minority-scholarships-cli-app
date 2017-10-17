5b125320a38d0b0781ceb5e58340423c4219f175
Lory NPC <loryleenbatioco@gmail.com>
2017-10-16 23:27:13 -0700

    add how_to_print_minority_groups to refactor print_minority_groups

--- lib/minority-scholarships-cli-app/cli.rb
+++ lib/minority-scholarships-cli-app/cli.rb
@@ -1,9 +1,6 @@
 class MinorityScholarshipsCliApp::CLI
 
   def call
-    puts ""
-    puts "preparing scholarships...".colorize(:blue)
-    puts ""
     load_data
     welcome
     menu
@@ -17,29 +14,42 @@ class MinorityScholarshipsCliApp::CLI
   end
 
   def load_data
+    puts ""
+    puts "loading scholarships...".colorize(:blue)
+    puts ""
     MinorityScholarshipsCliApp::Scraper.make_minority_groups
     MinorityScholarshipsCliApp::Scraper.scrape_scholarships
   end
 
-  def print_minority_groups(input = nil)
+  def print_minority_groups(input)
+    MinorityScholarshipsCliApp::MinorityGroups.all.each.with_index(1) do |ethnicity, index|
+      puts "[#{index}] ".bold.colorize(:green) + "#{ethnicity.name}"
+    end
+  end
+
+  def print_minority_groups_with_valid_selection(input)
+    MinorityScholarshipsCliApp::MinorityGroups.all.each.with_index(1) do |ethnicity, index|
+      if index == input.to_i
+        puts "[x] #{ethnicity.name.upcase} - #{ethnicity.awards} AWARDS - #{ethnicity.total} TOTAL".bold.colorize(:green)
+      else
+        puts "[#{index}] #{ethnicity.name}"
+      end
+    end
+  end
+
+  def input_error
+    puts ""
+    puts "[!] ".bold.colorize(:red) + "INPUT ERROR. Please enter a different command.".colorize(:red)
+  end
+
+  def how_to_print_minority_groups(input = nil)
     if input == nil
-      MinorityScholarshipsCliApp::MinorityGroups.all.each.with_index(1) do |ethnicity, index|
-        puts "[#{index}] ".bold.colorize(:green) + "#{ethnicity.name}"
-      end
+      print_minority_groups(input)
     elsif input.to_i > 0 && input.to_i < 6
-      MinorityScholarshipsCliApp::MinorityGroups.all.each.with_index(1) do |ethnicity, index|
-        if index == input.to_i
-          puts "[x] #{ethnicity.name.upcase} - #{ethnicity.awards} AWARDS - #{ethnicity.total} TOTAL".bold.colorize(:green)
-        else
-          puts "[#{index}] #{ethnicity.name}"
-        end
-      end
+      print_minority_groups_with_valid_selection(input)
     else
-      MinorityScholarshipsCliApp::MinorityGroups.all.each.with_index(1) do |ethnicity, index|
-        puts "[#{index}] ".colorize(:green) + "#{ethnicity.name}"
-      end
-      puts ""
-      puts "[!] ".bold.colorize(:red) + "INPUT ERROR. Please enter a different command.".colorize(:red)
+      print_minority_groups(input)
+      input_error
     end
   end
 
@@ -55,7 +65,7 @@ class MinorityScholarshipsCliApp::CLI
     end
   end
 
-  def display_next_page(input)
+  def view_more_info(input)
     if input.to_i > 0 && input.to_i < 6
       MinorityScholarshipsCliApp::MinorityGroups.all.each.with_index(1) do |ethnicity, index|
         if index == input.to_i
@@ -77,9 +87,9 @@ class MinorityScholarshipsCliApp::CLI
   def menu
     input = nil
     while input != "exit"
-      display_next_page(input)
+      view_more_info(input)
       puts "-------------------------------------------------------------------------"
-        print_minority_groups(input)
+        how_to_print_minority_groups(input)
         puts ""
         puts "enter the number of the category you would like to view.".colorize(:blue)
         puts "enter 'exit' to end the program.".colorize(:blue)

e93dc43172325b13f5fecaebf88e6005efd5c9f0
Lory NPC <loryleenbatioco@gmail.com>
2017-10-16 22:27:37 -0700

    delete display_error, instead add input error trigger to print_minority_groups

--- lib/minority-scholarships-cli-app/cli.rb
+++ lib/minority-scholarships-cli-app/cli.rb
@@ -22,16 +22,24 @@ class MinorityScholarshipsCliApp::CLI
   end
 
   def print_minority_groups(input = nil)
-    MinorityScholarshipsCliApp::MinorityGroups.all.each.with_index(1) do |ethnicity, index|
-      if input != nil
+    if input == nil
+      MinorityScholarshipsCliApp::MinorityGroups.all.each.with_index(1) do |ethnicity, index|
+        puts "[#{index}] ".colorize(:green) + "#{ethnicity.name}"
+      end
+    elsif input.to_i > 0 && input.to_i < 6
+      MinorityScholarshipsCliApp::MinorityGroups.all.each.with_index(1) do |ethnicity, index|
         if index == input.to_i
           puts "[x] ".colorize(:green) + "#{ethnicity.name} - #{ethnicity.awards} Awards Available - #{ethnicity.total} Total".colorize(:green)
         else
           puts "[#{index}] #{ethnicity.name}"
         end
-      else
+      end
+    else
+      MinorityScholarshipsCliApp::MinorityGroups.all.each.with_index(1) do |ethnicity, index|
         puts "[#{index}] ".colorize(:green) + "#{ethnicity.name}"
       end
+      puts ""
+      puts "[!] INPUT ERROR. Please enter a different command.".colorize(:red)
     end
   end
 
@@ -43,13 +51,6 @@ class MinorityScholarshipsCliApp::CLI
       puts "Info: #{scholarship.description}"
       puts "Link: " + "#{scholarship.url}".colorize(:blue)
       puts ""
-      puts ""
-    end
-  end
-
-  def display_error(input)
-    if input.to_i >= 6 && input != "exit" && input != nil
-      puts "[!] INPUT ERROR. Please enter a different command.".colorize(:red)
       puts ""
     end
   end
@@ -75,14 +76,15 @@ class MinorityScholarshipsCliApp::CLI
   def menu
     input = nil
     while input != "exit"
-        display_next_page(input)
-        puts "------------------------------------------------------------------------".bold
+      display_next_page(input)
+      puts "-------------------------------------------------------------------------"
+        puts ""
         print_minority_groups(input)
         puts ""
-        display_error(input)
         puts "enter the number of the category you would like to view.".colorize(:blue)
         puts "enter 'exit' to end the program.".colorize(:blue)
-      puts "------------------------------------------------------------------------".bold
+        puts ""
+      puts "-------------------------------------------------------------------------"
 
       input = gets.strip.downcase
       if input.to_i > 0 && input.to_i < 6
